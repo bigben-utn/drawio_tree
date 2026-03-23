795289ebca1ac267479c8592e742b6defb3edbf1
Ben Moreau <bigben.mro@gmail.com>
2026-03-23 13:24:30 -0300

    Agrego objeto Mapa()

--- spatial_tree.py
+++ spatial_tree.py
@@ -1,5 +1,29 @@
 from node import Node
 
+class Mapa():
+    def __init__(self, niveles: list[list[Node]]):
+        root = niveles[0][0]
+
+        self.x = root.data.long
+        self.y = (len(niveles) * 2) - 1
+
+        self.matrix = []
+        for y in range(0, self.y):
+            self.matrix.append([])
+            for x in range(0, self.x):
+                self.matrix[y].append(None)
+    
+    def add(self, node: Node, x, y):
+        self.matrix[x][y] = node
+        node.data.pos_absoluta = (x, y)
+
+    def __str__(self):
+        res = ""
+        for y in range(0, self.y):
+            res += str(self.matrix[y])
+            res += "\n"
+        return res
+
 def nodeUpdater(niveles: list[list[Node]]):
     for i in range(0, len(niveles)):
         nivel = niveles[len(niveles) - 1 - i]
